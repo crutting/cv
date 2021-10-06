e8b8cef10644445ce6a1a8822ebda75e613411ec
crutting <crutting@utah.gov>
2021-10-06 11:49:24 -0600

    Added a list of websites with source code and tutorials for building a professional website in R

---
+++ build tutorials.R
@@ -0,0 +1,9 @@
+https://www.emilyzabor.com/tutorials/rmarkdown_websites_tutorial.html
+
+https://towardsdatascience.com/get-your-own-website-online-in-four-steps-adef65abe8bd
+
+https://towardsdatascience.com/a-data-analysts-guide-to-creating-your-personal-website-with-r-f0079ba9b81c
+
+http://nickstrayer.me/datadrivencv/
+  
+https://statsandr.com/blog/how-to-create-a-timeline-of-your-cv-in-r/
