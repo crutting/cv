ea70b27b8114165894a72016716a4a63bf61c31c
crutting <crutting@utah.gov>
2022-03-31 19:40:08 -0600

    updated resume files

---
+++ resume.R
@@ -0,0 +1,128 @@
+Clark Ruttinger 
+
+Contact Information
+4028 S 1400 E
+SLC, UT, 84124
+ceruttinger@gmail.com
+801-205-1514
+
+Work Experience
+Utah Medical Education Council
+Interim, Executive Director 1.2022- present
+Director, Workforce Research 01.2018- 12.2021
+Supervise all research staff and interns.
+Management of all research projects for the Council.
+Recipient of IBM Healthcorps grant to model Primary Care workforce based on population health needs. 
+Provide high level expertise in research design, statistical analysis and emerging issues/trends.
+Design, development and management of data cleaning processes, data warehousing, modeling and visualization. 
+Design and maintain Amazon Cloud environment for research purposes
+Managed creation of data warehouse environment for storing supply data sets for analysis
+Set up an Rshiny server to publish interactive data visualizations
+Set up a neo4j graph database with web browser interface on a utah.gov domain to query and analyze state APCD data
+Responsible for survey design, data collection, authoring/editing of research papers, project reports, policy and position papers, articles and technical reports.
+Providing consultation, recommendations and support for executive level decision makers, educators administrators and researchers across the healthcare industry in Utah and nationally regarding medical workforce policy issues.
+Senior Institutional Research Analyst   01.2016 -2018
+Director of Utah’s Nursing Workforce Information Center.
+Gather survey data, analyze, present and advise on nurse education, supply and demand.
+Responsible for mentoring and direction of the work of 5 research analysts.
+Created the methodology and data linkage for a nursing employment demand analysis required in statute by the legislature beginning in 2020. 
+Research Analyst   01.2010 – 12.2015
+Consulting, research, surveying, analysis and policy recommendation development for all categories of the Utah medical workforce (ie: physicians, dentists, PAs, APRNs, pharmacists)
+Increased frequency of cyclical workforce reporting by 20%
+Secure management of all data received by the office from state agencies and medical training programs.
+Wrote VBA code to clean and organize data for analysis
+Maintain the state of Utah’s Physician Resident Rotation Database; allowing for resident training hospitals to assure maximum reimbursement from Medicare for Graduate Medical Education training.
+Wrote VBA code to format and combine multiple large spreadsheets into a standard format.
+
+Valley Mental Health/Hopeful Beginnings
+Respite Care Provider   06.2006 – 06.2015
+Provided weekly behavior modeling, opportunities for social interaction and activities for youth diagnosed with behavioral disorders receiving services through Valley Mental Health and Hopeful Beginnings. 
+
+State of Utah- Department of Workforce Services
+Administrative Hearing Specialist   03.2008 – 12.2009
+Acted as presiding officer in the review and representation of the State in administrative action against individuals who had incurred a public assistance overpayment.
+Created macros to increase speed and accuracy of processing administrative actions
+Reviewed the work of an Overpayment Calculation Analyst for accuracy, correct administrative legal classification and proper supporting evidence to order an overpayment of welfare benefits.
+Pulled welfare benefit data using an SQL graphic user interface.  
+Responsible for explanation and presentation of complex welfare policies within the setting of an administrative hearing in front of an Administrative Law Judge to overpaid individuals and counsel.
+Eligibility Specialist   06.2005 – 02.2008
+Interpreted federal and state welfare policies including TANF, Food Stamps and Child Care to approve application for benefits.
+Medicaid specialist at an off-site community health clinic.
+
+The Road Home
+Grants Coordinator   10.2003 – 01.2005
+Researched, wrote, submitted and reported on over 150 established grants per year from foundations, corporations and government 
+Tracked the receipt and recognition of grant and donation monies through donor database software. 
+Assisted Development staff with the planning of all yearly fundraisers, direct mailings, volunteer coordination, updating agency website and education of the public through tours and speaking engagements.
+Housing Case Manager   10.2002 – 10.2003
+Provided Case management services to between 20 and 30 clients at any time
+Assisted clients with referrals to any community assistance programs available
+Coordination with community agencies to assure clients receipt of services.
+
+Americorps
+Wasatch Homeless Health Care- Care Coordinator    2002
+Interviewed patients to assess eligibility for private clinic and public health care programs
+Developed and implemented procedures and training for the care coordination position.
+Assisted clients in arranging to receive medical services at no cost through local medical providers who were willing to voluntarily donate their professional services.
+Education
+Master of Business Administration
+University of Utah   Certificate in Information Systems-  2017
+
+Master of Public Administration
+University of Utah    Emphasis in program evaluation and policy analysis- 2007
+
+Utah Dispute Resolution- Utah State Bar
+40 hours of Basic Mediation Training- 2008
+
+BA, Political Science
+University of Utah    certificate in International Relations 1998-2002
+
+BA, Spanish
+University of Utah    1998- 2002
+
+Volunteer Experience
+National Forum of State Nursing Workforce Centers 2018-2020
+Board Member 
+Board President 2019
+Sharing best practices in policy, planning and advocacy of healthcare workforce issues across member states. Focused on supporting nursing in the context of improving the nursing role in the interprofessional care team. Working on data driven decision making tools to support better resource allocation for healthcare policy makers at the state and national level. 
+http://nursingworkforcecenters.org/
+  
+  U.S. Department of Health and Human Services Health Resources and Services Division (HRSA)
+Grant Reviewer   01.2011- present
+Objectively evaluate and score grant applications against published evaluation criteria in order to advise the federal government on selecting the best programs for funding from a competitive group of applicants. 
+Experience reviewing on a dozen funding opportunities and hundreds of applications.
+
+Salt Lake City Planning Commission
+Planning Commissioner    11.2011- 2019
+Adopt and act on any changes to the City’s master plans, small area plans, special studies, zoning ordinances, and zoning district maps, approving planned unit developments, conditional uses and certain subdivisions. 
+Commission Chair for the 2015 year.
+
+Community Action Program
+Landlord/Tenant Mediator   2009
+Mediated between landlords and tenants in pre and post eviction filing processes in issues of repairs and maintenance, late rent, lease violations, utilities, privacy issues etc
+
+Publications
+Journal Articles
+Bounsanga et al, Using machine learning to identify factors associated with practice location of
+the healthcare workforce, Rural and Remote Health, 2022
+
+Smiley et al, The 2020 National Nursing Workforce Survey, Journal of Nursing Regulation, 2020
+post publication dashboard I designed/built in R shiny https://clarkruttinger.shinyapps.io/draftboard/
+  
+  Published Analysis
+Utah Medical Education Council (2021). Utah’s Mental Health  Workforce, 2021. Salt Lake City, UT.
+Utah Medical Education Council (2020). Utah’s Physician  Workforce, 2020. Salt Lake City, UT.
+Utah Medical Education Council (2020). Utah Supply of Registered Nurses and Licensed Practical Nurses, 2020. Salt Lake City, UT.
+Utah Medical Education Council (2020). Utah Nursing Demand 2020. Salt Lake City, UT.
+Utah Medical Education Council (2017). Utah’s Advanced Practice Registered Nurse Workforce,
+2017: A Study of the Supply and Distribution of APRNs in Utah. Salt Lake City, UT.
+Utah Medical Education Council (2016). Utah’s Physician Workforce, 2016: A Study of the
+Supply and Distribution of Physicians in Utah. Salt Lake City, UT.
+Utah Medical Education Council (2016). Supply of Nurses in Utah: The 2016 Survey Utah’s
+Registered Nurses. Salt Lake City, UT.
+Utah Medical Education Council (2013). Utah’s Advanced Practice Registered Nurse Workforce,
+2013: A Study of the Supply and Distribution of APRNs in Utah. Salt Lake City, UT.
+
+References and list of conference presentations and lectures available on request
+
+
